63b6b23b380ae2b5146b685c8501d8bfaccc622d
AlexWong <wongqianjie1234@gmail.com>
2019-07-07 15:47:56 +0800

    Updated songs Details

--- js/Player.js
+++ js/Player.js
@@ -5,6 +5,10 @@ var volumeIcon = document.getElementById("volume");
 var volumeSlider = document.getElementById("volumeRange");
 var startTime = document.getElementById("start");
 var endTime = document.getElementById("end");
+var previousIcon = document.getElementById("previous");
+var nextIcon = document.getElementById("next");
+var songLeftContanier = document.getElementById("song-Title");
+var songPicture = document.getElementById("songPicture");
 var isPlaying = false;
 var seeking = false;
 var seekTo;
@@ -32,7 +36,7 @@ function getSongs(){
   var ulClearFix = document.getElementById("ulClearFix");
   ulClearFix.innerHTML = "";
   var output="";
-  for (var songsList=0; songsList < 4; songsList++){
+  for (var songsList = 0; songsList <= 51; songsList++){
     output += '<li class="col-md-2 col-sm-3 col-xs-4">';
     output += '<div class="songPicLoading" style="background-image:url(image/Chinese/' + songsList + '.jpg); " onclick="addAudioSource(' + songsList + ')"></div>';
     output += '<div class="title">';
@@ -53,12 +57,22 @@ function addAudioSource(input){
   }
   else if(currentPlaying === audioTag.currentSrc){
     audioTag.pause();
-    audioTag.setAttribute("src", 'https://docs.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '')
+    startTime.style.color = "grey";
+    endTime.style.color = "grey";
+    previous.style.color = "grey";
+    playPause.style.color = "grey";
+    next.style.color = "grey";
+    startTime.innerHTML = "--:--";
+    endTime.innerHTML = "--:--";
+    audioTag.setAttribute("src", 'https://drive.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '')
     console.log(songs[0].list[input].name);
   }
-  audioTag.innerHTML = '<source src="https://docs.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '">';
+  audioTag.innerHTML = '<source src="https://drive.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '">';
   playPause.firstElementChild.innerText = "pause";
-  currentPlaying = 'https://docs.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '';
+  currentPlaying = 'https://drive.google.com/uc?export=download&id=' + songs[0].list[input].driveUrl + '';
+  songPicture.style.backgroundImage = "url('image/Chinese/" + input + ".jpg')";
+  songLeftContanier.innerText = songs[0].list[input].name;
+  songLeftContanier.style.color = "white";
   audioTag.play();
 }
 
@@ -81,21 +95,25 @@ function seekSong(){
 }
 
 function seekSongUpdate(){
-  var newTime = audioTag.currentTime * (100 / audioTag.duration);
-  juiceBarDuration.value = newTime;
-  startTime.style.color = "white";
-  endTime.style.color = "white";
-  console.log(juiceBarDuration.value);
-  var currentMinutes = Math.floor(audioTag.currentTime / 60);
-  var currentSeconds = Math.floor(audioTag.currentTime - currentMinutes * 60);
-  var durationMinutes = Math.floor((audioTag.duration - audioTag.currentTime) / 60);
-  var durationSeconds = Math.floor((audioTag.duration - audioTag.currentTime) - durationMinutes * 60);
-  if(currentMinutes < 10){ currentMinutes = "0" + currentMinutes;};
-  if(currentSeconds < 10){ currentSeconds = "0" + currentSeconds;};
-  if(durationMinutes < 10){ durationMinutes = "0" + durationMinutes;};
-  if(durationSeconds < 10){ durationSeconds = "0" + durationSeconds;};
-  startTime.innerHTML = currentMinutes + ":" + currentSeconds;
-  endTime.innerHTML = durationMinutes + ":" + durationSeconds;
+  if(isNaN(audioTag.currentTime) === false && isNaN(audioTag.duration) === false){
+    var newTime = audioTag.currentTime * (100 / audioTag.duration);
+    juiceBarDuration.value = newTime;
+    startTime.style.color = "white";
+    endTime.style.color = "white";
+    previous.style.color = "white";
+    playPause.style.color = "white";
+    next.style.color = "white";
+    var currentMinutes = Math.floor(audioTag.currentTime / 60);
+    var currentSeconds = Math.floor(audioTag.currentTime - currentMinutes * 60);
+    var durationMinutes = Math.floor((audioTag.duration - audioTag.currentTime) / 60);
+    var durationSeconds = Math.floor((audioTag.duration - audioTag.currentTime) - durationMinutes * 60);
+    if(currentMinutes < 10){ currentMinutes = "0" + currentMinutes;};
+    if(currentSeconds < 10){ currentSeconds = "0" + currentSeconds;};
+    if(durationMinutes < 10){ durationMinutes = "0" + durationMinutes;};
+    if(durationSeconds < 10){ durationSeconds = "0" + durationSeconds;};
+    startTime.innerHTML = currentMinutes + ":" + currentSeconds;
+    endTime.innerHTML = durationMinutes + ":" + durationSeconds;
+  }
 }
 
 
@@ -104,13 +122,28 @@ function mute(){
   if(audioTag.muted){
     audioTag.muted = false;
     volumeIcon.firstElementChild.innerText = "volume_up";
+    volumeSlider.value = 100;
   }
   else{
     audioTag.muted = true;
-    volumeIcon.firstElementChild.innerText = "volume_mute";
+    volumeIcon.firstElementChild.innerText = "volume_off";
+    volumeSlider.value = 0;
   }
 }
 
 function setVolume(){
   audioTag.volume = volumeSlider.value/100;
+  if (volumeSlider.value < 1){
+    volumeIcon.firstElementChild.innerText = "volume_mute";
+    audioTag.muted = true;
+  }
+  else if(volumeSlider.value <= 50 && volumeSlider.value >= 1){
+    volumeIcon.firstElementChild.innerText = "volume_down";
+    console.log(volumeSlider.value);
+    audioTag.muted = false;
+  }
+  else if(volumeSlider.value <= 100 && volumeSlider.value > 50){
+    volumeIcon.firstElementChild.innerText = "volume_up";
+    audioTag.muted = false;
+  }
 }
